440d9c0d6902e3eff2408c7e1db255fcc3d30b2c
Dan Waseen <dan.waseen1@gmail.com>
2016-10-31 17:55:25 -0500

    Test Comment

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TheLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.red
+        TheLabel.text = "Hello There"
+    }
+    @IBAction func button_tapped(_ sender: AnyObject) {
+        TheLabel.text = "Hello Again"
+    }
+    @IBAction func button2_tapped(_ sender: AnyObject) {
+        TheLabel.text = "Buttons are Cool"
     }
 
     override func didReceiveMemoryWarning() {
